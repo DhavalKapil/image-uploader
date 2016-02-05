5c59b4c0103baae0ace5268591c2e1b541078446
Dhaval Kapil <me@dhavalkapil.com>
2016-02-05 19:23:04 +0530

    Updated sample example

--- example/view.php
+++ example/view.php
@@ -6,7 +6,7 @@ try
 {
   $imageUpload = new ImageUpload($_FILES, "../upload", "random_salt");
 
-  $res = $imageUpload->upload("my_image", "my_id");
+  $res = $imageUpload->serve("my_id");
 
   var_dump($res);
 }
